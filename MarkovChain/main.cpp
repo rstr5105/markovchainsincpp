0ffd488fa955759bc08b6aa553681c5d1374f8a5
Robert Straitt <rstr5105@gmail.com>
2014-08-28 13:07:03 -0700

    Initial Commit

---
+++ MarkovChain/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+int main(){
+	printf("Hello World!");
+	
+
+	printf("\n\n\n\n\n\n\n\nPress Enter/Return to Continue!");
+	cin.get();
+
+}
